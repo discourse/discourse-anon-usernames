5c1c4661ef220cb472fe0c3a67c8b4c69a058f21
Jarek Radosz <jarek@cvx.dev>
2026-01-29 14:55:29 +0100

    DEV: Update linting and add types (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ DiscourseAnonUsernames::Engine.routes.draw do
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::DiscourseAnonUsernames::Engine, at: "discourse-anon-usernames" }
+Discourse::Application.routes.draw do
+  mount ::DiscourseAnonUsernames::Engine, at: "discourse-anon-usernames"
+end
